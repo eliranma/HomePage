8d4b40da28ed160c069c9a49bc5ff41d115da85f
eliran <manzeli@me.com>
2020-10-16 22:41:15 +0300

    first

---
+++ homepage.js
@@ -0,0 +1,24 @@
+const greeting = () =>{
+    const currentTime = new Date();
+    const currentHour = currentTime.getHours();
+    var currentGreeting = "";
+    if(currentHour >= 5 && currentHour <= 11 ){
+        document.querySelector('.hello').innerText = "בוקר טוב אלירן";
+        } else if(currentHour >= 12 && currentHour <= 15 ){
+            document.querySelector('.hello').innerText = "צהריים טובים אלירן"
+        }else if(currentHour >= 16 && currentHour <= 18 ){
+            document.querySelector('.hello').innerText = "<h1>שלום</h1>";
+        }else if(currentHour >= 19 && currentHour <= 22 ){
+            document.querySelector('.hello').innerText = "ערב טוב אלירן";
+        }else if(currentHour >= 23 && currentHour <= 4 ){
+            document.querySelector('.hello').innerText  = "לילה טוב אלירן";
+        }
+    };
+    greeting();
+// const searchGoogle = (e) =>{
+// e = document.querySelector(google,search);
+// }
+//TODO: const computerSearch
+//TODO: const youtubeSearch
+//TODO: const newsFeed of random(3 twitter, 3 facebook post, 3 instagram post)
+//TODO: const news most new articles
